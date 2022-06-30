a4e062fa4f1c2746d4c4109e7aff48e88ec2a775
jackkheir <jack.kheir@unit9.com>
2022-06-30 13:44:29 +0100

    TestCommit

--- app.py
+++ app.py
@@ -84,3 +84,5 @@ def appl():
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
     app.run(debug=False, port=server_port, host='0.0.0.0')
+
+#THIS COMMENT WAS MADE BY JACK
