8a889e64b7eb227c51da88c0d6bf0d395ca30994
Tibor <satibcsi4500@gmail.com>
2022-07-01 08:30:25 +0000

    asd2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import requests
 
 app = Flask(__name__)
 connector = Connector()
-#asd
+#asdasd
 def getconn() -> pymysql.connections.Connection:
     conn: pymysql.connections.Connection = connector.connect(
         "turnkey-banner-354610:europe-west6:example",
